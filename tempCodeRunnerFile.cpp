76174e5169f2965f1ee36949b01c9bd54279dc4d
Chay <chayangkul_c@cmu.ac.th>
2023-12-22 16:08:53 +0700

    lab11

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+#include<string>
+
+using namespace std ;
+int main(){
+    string text = "NAJJ" ;
+    if(text[0] = "N")
+    cout << "K" ;
+}
